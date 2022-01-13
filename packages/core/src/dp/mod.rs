3cc7f5f4c81b9da6ab140d6e5c486233d5892cc7
Rodrigo Racanicci <rracanicci@microsoft.com>
2022-01-13 15:45:27 -0300

    adding structures to perform selection using dp percentiles

--- packages/core/src/dp/mod.rs
+++ packages/core/src/dp/mod.rs
@@ -1,3 +1,7 @@
 /// Defines functions and structures to create
 /// normal distributions using the Analytic Gaussian Mechanism
 pub mod analytic_gaussian;
+
+/// Functions and structures to perform DP selection
+/// for a vector of integers using percentiles
+pub mod percentile;
