3a83c26eb1d2af0878f1ad1a35f501681d5969f2
Rodrigo Racanicci <rracanicci@microsoft.com>
2022-07-22 10:17:39 -0300

    exposing set_number_of_threads in sdssynth

--- packages/lib-sdssynth/src/utils/mod.rs
+++ packages/lib-sdssynth/src/utils/mod.rs
@@ -1,10 +1,13 @@
 use pyo3::prelude::*;
 
 mod logger;
+mod threading;
 
 pub use logger::*;
+pub use threading::*;
 
 pub(crate) fn register(py: Python<'_>, m: &PyModule) -> PyResult<()> {
     logger::register(py, m)?;
+    threading::register(py, m)?;
     Ok(())
 }
